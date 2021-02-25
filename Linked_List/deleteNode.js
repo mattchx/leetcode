7c84e3ae9283769398dfd4125f1eb30579a02e56
Matthew <55170649+mattchx@users.noreply.github.com>
2021-02-25 17:07:54 -0500

    Create deleteNode.js

---
+++ Linked_List/deleteNode.js
@@ -0,0 +1,18 @@
+//237. Delete Node in a Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    console.log(node.val, node.next.val)
+    node.val = node.next.val
+    node.next = node.next.next
+    
+};
