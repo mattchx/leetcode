54da7a76b3b1105aa8170eaaf56e1106db470d90
Matthew <55170649+mattchx@users.noreply.github.com>
2021-02-26 18:29:07 -0500

    Create reverseList.js

---
+++ Linked_List/reverseList.js
@@ -0,0 +1,26 @@
+// Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null
+    let next = null
+    
+    while(head !== null){
+        
+        next = head.next
+        head.next = prev
+        console.log(head)
+        prev = head
+        head = next
+    }
+    return prev
+};
